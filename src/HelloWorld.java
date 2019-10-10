dec04a60d1e1c98f17a8853acb7555f4b4dbc652
VALENCIA <lvalencia19@cnm.edu>
2019-10-10 14:39:06 -0600

    Add enum class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Burgers burgers = null;
+        
+        System.out.println("Hello World" + burgers.BACON);
     }
 }
